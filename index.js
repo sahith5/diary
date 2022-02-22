9c919401cc29f9a08139a1040634a162959cdfaa
sahith <bandasahithreddy@gmail.com>
2022-02-22 23:57:52 +0530

    message addeddd

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const express=require("express");
 const app=express();
-const port=process.env.port;
-app.listen(port||3000,function()
+const port=process.env.PORT|| 3000;
+const path=require('path');
+app.listen(port,function()
 {
     console.log("app staerted");
 })
 
 app.get("/",function(req,res)
 {
-    res.sendFile(__dirname+"/frontend/login.html");
+    res.sendFile(path.join(__dirname+"/frontend/login.html"));
 })
 app.use(express.static(__dirname+"/frontend/images"));
 
 app.get("/write",function(req,res)
 {
-    res.sendFile(__dirname+"/frontend/write.html");
+    res.sendFile(path.join(__dirname+"/frontend/write.html"));
 })
